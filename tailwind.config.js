b4d0a0de840ca4e55846eb039a23e124478af8e1
The-ARJ <joshiaayush871@gmail.com>
2023-04-16 19:46:53 +0545

    daisly ui package removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,5 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
-  daisyui: {
-    themes: ["emerald"],
-},
-  plugins: [require("daisyui")],
+  plugins: [],
 }
